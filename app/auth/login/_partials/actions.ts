776aed54fe1c27f9234643f2483649ddcb3020bb
momo391 <ia7636hs@gmail.com>
2024-12-22 11:11:23 +0100

    log in completed

---
+++ app/auth/login/_partials/actions.ts
@@ -0,0 +1,78 @@
+"use server";
+
+import { z } from "zod";
+import { NeonDbError } from "@neondatabase/serverless";
+import { db } from "@/db";
+
+import { eq } from "drizzle-orm";
+import { verifyPassword } from "@/lib/server/password";
+import { createSession, generateSessionToken } from "@/lib/server/session";
+import { setSessionTokenCookie } from "@/lib/server/cookies";
+import { logInSchema } from "@/schema/log-in";
+import { userTable } from "@/db/schema/user";
+import { Session } from "@/db/schema/session";
+
+export const login = async (
+  values: z.infer<typeof logInSchema>
+): Promise<LogInResult> => {
+  try {
+    const email: string = values.email;
+
+    const result = await db
+      .select({ password: userTable.password, user_id: userTable.id })
+      .from(userTable)
+      .where(eq(userTable.email, email));
+
+    if (result.length < 1)
+      return {
+        where: "email",
+        message: "account not found",
+      };
+
+    const { password: hash, user_id } = result[0];
+
+    if (!hash)
+      return {
+        where: "password",
+        message: "account corrupted",
+      };
+
+    const isUser = await verifyPassword(hash, values.password);
+    if (!isUser)
+      return {
+        where: "password",
+        message: "wrong credentials",
+      };
+
+    const token: string = generateSessionToken();
+    const session: Session = await createSession(token, user_id);
+
+    if (!session)
+      return {
+        where: "Database",
+        message: "Can't create session",
+      };
+
+    await setSessionTokenCookie(token, session.expires_at);
+
+    return {
+      where: null,
+      message: null,
+    };
+  } catch (err) {
+    if (err instanceof NeonDbError) {
+      return {
+        where: "Database",
+        message: "Something went wrong",
+      };
+    }
+    return {
+      where: "Server",
+      message: "Something went wrong",
+    };
+  }
+};
+
+export type LogInResult =
+  | { where: string; message: string }
+  | { where: null; message: null };
---
+++ app/auth/login/_partials/form.tsx
@@ -0,0 +1,104 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { Icon } from "@iconify/react";
+
+import { useState } from "react";
+import { login, LogInResult } from "./actions";
+
+import { redirect } from "next/navigation";
+import { logInSchema } from "@/schema/log-in";
+
+export const LogInForm = () => {
+  const [loading, setLoading] = useState(false);
+  const form = useForm<z.infer<typeof logInSchema>>({
+    resolver: zodResolver(logInSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+  const { setError } = form;
+
+  async function onSubmit(values: z.infer<typeof logInSchema>) {
+    console.log(values);
+
+    setLoading(true);
+    const { where, message }: LogInResult = await login(values);
+    setLoading(false);
+
+    if (where === "password" || where === "email")
+      setError(where, { type: "deps", message });
+    else if (where === "Database" || where === "Server")
+      setError("root", { type: "deps", message });
+    else redirect("/");
+  }
+
+  return (
+    <>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder="email@gmail.com" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input placeholder="********" type="password" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <div className="flex  justify-between">
+            <Link
+              className={cn(buttonVariants({ variant: "link" }))}
+              href={"/auth/signup"}
+            >
+              don't have an account ?
+            </Link>
+
+            <Button type="submit" disabled={loading}>
+              {loading ? (
+                <Icon icon="tabler:loader-2" className="animate-spin" />
+              ) : null}
+              log in
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </>
+  );
+};
