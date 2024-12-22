a2a1e8606fd639ed9f2de9df878283fd76d05b82
momo391 <ia7636hs@gmail.com>
2024-12-22 10:46:36 +0100

    db updated

--- db/schema/user.ts
+++ db/schema/user.ts
@@ -5,6 +5,12 @@ export const userTable = pgTable("users", {
   id: uuid("id").primaryKey().defaultRandom(),
   email: text("email").unique().notNull(),
   password: varchar("password").notNull(),
+
+  last_name: varchar("last_name", { length: 50 }).notNull(),
+  first_name: varchar("first_name", { length: 50 }).notNull(),
+  username: varchar("username", { length: 50 }).unique().notNull(),
+  image: text("image"),
+  cover: text("cover"),
 });
 
 export type User = InferInsertModel<typeof userTable>;
